58262a470691ffc193c7ab68a82eb1e1b8272eaf
Nathan LEFETEY <lefetey.nathan@gmail.com>
2024-01-08 16:33:46 +0100

    :bug: Fix popup animation position bug

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import Popup from './src/popup.js';
+import Popup from "./src/popup.js";
 
 const popup = new Popup({
- text: 'Your text here',
- position: 'center',
- animationIn: 'fadeRight',
- animationOut: 'fadeRight',
- contentStyle: { backgroundColor: '#ffffff', padding: '20px' }
+  text: "Your text here",
+  position: "center",
+  animationIn: "fadeRight",
+  animationOut: "fadeRight",
+  contentStyle: { backgroundColor: "#ffffff", padding: "20px" },
 });
 popup.show();
