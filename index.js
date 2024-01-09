132a67c492680d2f9680d02dc62849ddc574c68e
Nathan LEFETEY <lefetey.nathan@gmail.com>
2024-01-09 10:18:39 +0100

    :bug: Fix animation
    Fix animation bug on close popup click

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ import Popup from "./src/popup.js";
 const popup = new Popup({
   text: "Your text here",
   position: "center",
-  animationIn: "fadeRight",
-  animationOut: "fadeRight",
+  animationIn: "fadeIn",
+  animationOut: "fadeInReverse",
   contentStyle: { backgroundColor: "#ffffff", padding: "20px" },
 });
 popup.show();
